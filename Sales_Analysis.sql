a8d36b418d3b52f9672a51eead2cdd5c0464337c
Godwin <godwinogele96@gmail.com>
2025-03-04 09:49:46 +0000

    Update Sales_Analysis.sql

--- Sales_Analysis.sql
+++ Sales_Analysis.sql
@@ -26,3 +26,40 @@ SELECT YEAR(Transaction_Date) AS Year, MONTH(Transaction_Date) AS Month, SUM(Qua
 FROM Sales
 GROUP BY YEAR(Transaction_Date), MONTH(Transaction_Date)
 ORDER BY Year, Month;
+
+--Purchase frequency by gender
+SELECT 
+    Customer_Gender, 
+    COUNT(Transaction_ID) AS Purchase_Frequency
+FROM Sales
+GROUP BY Customer_Gender
+ORDER BY Purchase_Frequency DESC;
+
+--Purchase frequency by age groups
+SELECT 
+    CASE 
+        WHEN Customer_Age BETWEEN 18 AND 25 THEN '18-25'
+        WHEN Customer_Age BETWEEN 26 AND 35 THEN '26-35'
+        WHEN Customer_Age BETWEEN 36 AND 45 THEN '36-45'
+        WHEN Customer_Age BETWEEN 46 AND 60 THEN '46-60'
+        ELSE '60+' 
+    END AS Age_Group, 
+    COUNT(Transaction_ID) AS Purchase_Frequency
+FROM Sales
+GROUP BY 
+    CASE 
+        WHEN Customer_Age BETWEEN 18 AND 25 THEN '18-25'
+        WHEN Customer_Age BETWEEN 26 AND 35 THEN '26-35'
+        WHEN Customer_Age BETWEEN 36 AND 45 THEN '36-45'
+        WHEN Customer_Age BETWEEN 46 AND 60 THEN '46-60'
+        ELSE '60+' 
+    END
+ORDER BY Purchase_Frequency DESC;
+
+--Purchase frequency by membership status
+SELECT 
+    Membership_Status, 
+    COUNT(Transaction_ID) AS Purchase_Frequency
+FROM Sales
+GROUP BY Membership_Status
+ORDER BY Purchase_Frequency DESC;
